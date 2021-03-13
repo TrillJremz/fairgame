00243e859a99091f4014de4d1032a31de88412d6
TrillJremz <79770259+TrillJremz@users.noreply.github.com>
2021-03-13 17:01:51 -0500

    Update _Amazon.bat

--- _Amazon.bat
+++ _Amazon.bat
@@ -1,1 +1,1 @@
-cmd /k pipenv run python app.py amazon --delay=1 --no-screenshots --checkshipping --used 
+pipenv run python app.py amazon --delay=1 --no-screenshots --checkshipping --used --no-image
